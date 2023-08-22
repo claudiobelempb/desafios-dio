b5a4c7ece26545d3ef25e5b8ff52470ab469cbac
claudiobelempb <claudio.c.lima@hotmail.com>
2023-08-22 10:18:36 -0300

    desafio controle deflux

---
+++ desafios/controleFluxo/src/controleflux/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package controleflux;
+
+public class ParametrosInvalidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String error) {
+		super(error);
+	}
+
+};
